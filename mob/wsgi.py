99c273099cfb5e2fb6505925fc7b0f0c85d3e96f
cippero <gilwein@gmail.com>
2018-06-11 14:44:53 -0700

    deployment

--- mob/wsgi.py
+++ mob/wsgi.py
@@ -15,9 +15,3 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mob.settings")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mob.settings")
-
-application = get_wsgi_application()
